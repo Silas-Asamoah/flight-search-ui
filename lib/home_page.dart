f2db0daa8fa2d6b9d3481966ebed2e5a4b7edfa9
Silas-Asamoah <silasbempong@gmail.com>
2020-04-08 22:52:24 +0000

    top buttons in the app bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flightsearch/rounded_button.dart';
 import 'package:flutter/material.dart';
 import 'air_asia_bar.dart';
 
@@ -9,8 +10,33 @@ class HomePage extends StatelessWidget{
       body: Stack(
         children: <Widget>[
           AirAsiaBar(height: 210.0),
+          Positioned.fill(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 40),
+                child: new Column(
+                  children: <Widget>[
+                    _buildButtonsRow(),
+                    Container()
+                  ],
+                ),
+              )
+          )
         ],
       )
     );
   }
+}
+
+Widget _buildButtonsRow(){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      children: <Widget>[
+        new RoundedButton(text: "KUMASI"),
+        new RoundedButton(text: "ACCRA"),
+        new RoundedButton(text: "TAKORADI", selected: true),
+      ],
+    ),
+  );
 }
